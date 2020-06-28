0f6515577f72c47c05fce45c6a81c3180e9914a7
yukishimoda <smdjiao@yahoo.co.jp>
2020-06-28 13:48:40 +0000

    validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,18 @@
 class TasksController < ApplicationController
     def index
-      @tasks = Task.all
+     @tasks = Task.all
     end
     
     def show
-      @tasks = Task.find(params[:id])
+     @tasks = Task.find(params[:id])
     end
     
     def new
-     @tasks = Task.new
+     @task = Task.new
     end
     
    def create
-     @task = Task.new(message_params)
+     @task = Task.new(task_params)
      if @task.save
        flash[:success] = 'Message が正常に投稿されました'
        redirect_to @task
@@ -23,17 +23,17 @@ class TasksController < ApplicationController
    end
     
     def edit
-        @task = Task.find(params[:id])
+        @tasks = Task.find(params[:id])
     end
     
     def update
      @task = Task.find(params[:id])
      if @task.update(task_params)
-       flash[:success] = 'Message は正常に更新されました'
-       redirect_to @task
+        flash[:success] = 'Message は正常に更新されました'
+        redirect_to @task
      else
-       flash.now[:danger] = 'Message は更新されませんでした'
-       render :edit
+        flash.now[:danger] = 'Message は更新されませんでした'
+        render :edit
      end
     end
     
