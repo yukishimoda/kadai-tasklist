aa265fd688b2edd32e23ec77ab8d61b8e30b27a6
yukishimoda <smdjiao@yahoo.co.jp>
2020-06-28 12:54:04 +0000

    CRUD messages

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+    def index
+      @tasks = Task.all
+    end
+    
+    def show
+      @tasks = Task.find(params[:id])
+    end
+    
+    def new
+     @tasks = Task.new
+    end
+    
+   def create
+     @task = Task.new(message_params)
+     if @task.save
+       flash[:success] = 'Message が正常に投稿されました'
+       redirect_to @task
+     else
+       flash.now[:danger] = 'Message が投稿されませんでした'
+       render :new
+     end
+   end
+    
+    def edit
+        @task = Task.find(params[:id])
+    end
+    
+    def update
+     @task = Task.find(params[:id])
+     if @task.update(task_params)
+       flash[:success] = 'Message は正常に更新されました'
+       redirect_to @task
+     else
+       flash.now[:danger] = 'Message は更新されませんでした'
+       render :edit
+     end
+    end
+    
+    def destroy
+      @task = Task.find(params[:id])
+      @task.destroy
+      flash[:success] = 'Message は正常に削除されました'
+      redirect_to task_url
+    end
+    
+    private
+    
+    def task_params
+        params.require(:task).permit(:content)
+    end
+    
+end
