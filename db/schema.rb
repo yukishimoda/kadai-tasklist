144fd1b822791b4abb0dd3212185aa7cd45f3df9
yukishimoda <smdjiao@yahoo.co.jp>
2020-07-20 13:32:42 +0000

    add git-logo image

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2020_07_16_122958) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.string "status"
   end
 
